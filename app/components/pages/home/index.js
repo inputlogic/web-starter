14b5f3e11ad9c015c522b1751f53ed4ffc017d7e
Cory Schadt <cory@inputlogic.ca>
2018-02-08 08:54:03 -0800

    Add basic table styles

--- app/components/pages/home/index.js
+++ app/components/pages/home/index.js
@@ -81,7 +81,7 @@ const Home = ({url, modals = {}}) =>
         </Form>
       </Card>
       <Row><h2>Grid</h2></Row>
-      <Row>{range(1, 4).map((n) =>
+      <Row>{range(0, 4).map((n) =>
         <Column>
           <Card>{n}</Card>
         </Column>
@@ -91,6 +91,7 @@ const Home = ({url, modals = {}}) =>
           <Card>{n}</Card>
         </Column>
       )}</Row>
+
     </div>
     <div className='spaced'>
       <Carousel>
@@ -99,6 +100,70 @@ const Home = ({url, modals = {}}) =>
         , ['fff', 'a7c', '09d', '411', '111'])}
       </Carousel>
     </div>
+
+    <div className='container'>
+      <h2>Styled Table</h2>
+
+      <table className='styled'>
+        <tr>
+          <th>Company</th>
+          <th>Contact</th>
+          <th>Country</th>
+        </tr>
+        <tr>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Germany</td>
+        </tr>
+        <tr>
+          <td>Berglunds snabbköp</td>
+          <td>Christina Berglund</td>
+          <td>Sweden</td>
+        </tr>
+        <tr>
+          <td>Centro comercial Moctezuma</td>
+          <td>Francisco Chang</td>
+          <td>Mexico</td>
+        </tr>
+        <tr>
+          <td>Ernst Handel</td>
+          <td>Roland Mendel</td>
+          <td>Austria</td>
+        </tr>
+        <tr>
+          <td>Island Trading</td>
+          <td>Helen Bennett</td>
+          <td>UK</td>
+        </tr>
+        <tr>
+          <td>Königlich Essen</td>
+          <td>Philip Cramer</td>
+          <td>Germany</td>
+        </tr>
+        <tr>
+          <td>Laughing Bacchus Winecellars</td>
+          <td>Yoshi Tannamuri</td>
+          <td>Canada</td>
+        </tr>
+        <tr>
+          <td>Magazzini Alimentari Riuniti</td>
+          <td>Giovanni Rovelli</td>
+          <td>Italy</td>
+        </tr>
+        <tr>
+          <td>North/South</td>
+          <td>Simon Crowther</td>
+          <td>UK</td>
+        </tr>
+        <tr>
+          <td>Paris spécialités</td>
+          <td>Marie Bertrand</td>
+          <td>France</td>
+        </tr>
+      </table>
+
+    </div>
+
   </Page>
 
 export default Home
