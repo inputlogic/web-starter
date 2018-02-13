7db3cdf3f407d3b7cdfc40c83a5506e141d02432
Adrian Unger <dev@staydecent.ca>
2018-02-13 11:06:28 -0800

    Add missing util for pagination.

---
+++ app/util/updateQuery.js
@@ -0,0 +1,11 @@
+import queryString from 'query-string'
+
+// @TODO: This does not preserve order of search parms. This may lead to
+// withRequests urls not matching because:
+//  `?a=1&b=2` may end up as `?b=2&a=1`
+export const updateQuery = (queries) => {
+  const existingParams = queryString.parse(window.location.search)
+  return window.location.pathname + `?${queryString.stringify({...existingParams, ...queries})}`
+}
+
+export default updateQuery
