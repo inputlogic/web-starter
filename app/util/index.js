498ffe9739776ce22e2bdc27895149c33ca8ceda
Adrian Unger <dev@staydecent.ca>
2018-03-02 11:09:10 -0800

    Remove generic utils that can be part of wasmuth.

--- app/util/index.js
+++ app/util/index.js
@@ -2,14 +2,10 @@ import apiUrl from './apiUrl'
 import apiUrlFromState from './apiUrlFromState'
 import compose from './compose'
 import connect from './connect'
-import deepClone from './deepClone'
 import getRouteIdFromState from './getRouteIdFromState'
 import parseResults from './parseResults'
-import relativeTime from './relativeTime'
 import request from './request'
 import {urlFor} from './route'
-import slugify from './slugify'
-import toType from './toType'
 import updateQuery from './updateQuery'
 import uploadImage from './uploadImage'
 import withRequest from './withRequest'
@@ -20,14 +16,10 @@ export default {
   apiUrlFromState,
   compose,
   connect,
-  deepClone,
   getRouteIdFromState,
   parseResults,
-  relativeTime,
   request,
   urlFor,
-  slugify,
-  toType,
   updateQuery,
   uploadImage,
   withRequest,
