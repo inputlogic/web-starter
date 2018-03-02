94bc474c7f47cbc7f37e7702f77dabfa0b2148d7
Adrian Unger <dev@staydecent.ca>
2018-03-02 11:05:38 -0800

    Export utils.

---
+++ app/util/index.js
@@ -0,0 +1,35 @@
+import apiUrl from './apiUrl'
+import apiUrlFromState from './apiUrlFromState'
+import compose from './compose'
+import connect from './connect'
+import deepClone from './deepClone'
+import getRouteIdFromState from './getRouteIdFromState'
+import parseResults from './parseResults'
+import relativeTime from './relativeTime'
+import request from './request'
+import {urlFor} from './route'
+import slugify from './slugify'
+import toType from './toType'
+import updateQuery from './updateQuery'
+import uploadImage from './uploadImage'
+import withRequest from './withRequest'
+import withState from './withState'
+
+export default {
+  apiUrl,
+  apiUrlFromState,
+  compose,
+  connect,
+  deepClone,
+  getRouteIdFromState,
+  parseResults,
+  relativeTime,
+  request,
+  urlFor,
+  slugify,
+  toType,
+  updateQuery,
+  uploadImage,
+  withRequest,
+  withState
+}
