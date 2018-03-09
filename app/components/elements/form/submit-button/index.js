df578560aa47e1252c5887bcab3f6c42ab0fe503
Adrian Unger <dev@staydecent.ca>
2018-03-09 09:38:26 -0800

    Fix SubmitButton regressions.
    - Should be disabled if no form data, or is submitting, or if parent
    passes disabled as a prop.
    - Should respect classNames

--- app/components/elements/form/submit-button/index.js
+++ app/components/elements/form/submit-button/index.js
@@ -1,18 +1,29 @@
-import {path} from 'wasmuth'
+import {pathOr, some} from 'wasmuth'
 import Ellipsis from '/components/elements/loader/ellipsis'
 import withState from '/util/withState'
 import Base from './base'
 
 export const SubmitButton = withState(
   'SubmitButton',
-  ({formState = {}}, {formName}) => ({submitting: path([formName, 'submitting'], formState)})
-)(({submitting, children, submittingText, formName}) =>
-  (!formName && console.warn('Submit button should have a formName')) ||
-  Base({
-    disabled: submitting,
-    type: 'submit',
-    children: submitting ? (submittingText || <Ellipsis />) : children
+  ({forms = {}, formErrors = {}, formState = {}}, {formName}) => ({
+    isDirty: some(x => x, forms[formName] || {}),
+    isSubmitting: pathOr(false, [formName, 'submitting'], formState)
   })
-)
+)(({
+  isDirty,
+  isSubmitting,
+  disabled,
+  submittingText,
+  className = '',
+  children
+}) => {
+  const isDisabled = disabled || (!isDirty || isSubmitting)
+  return Base({
+    className,
+    disabled: isDisabled,
+    type: 'submit',
+    children: isSubmitting ? (submittingText || <Ellipsis />) : children
+  })
+})
 
 export default SubmitButton
