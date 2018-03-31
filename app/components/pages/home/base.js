6b9704a5a002c76917363634ee48a1bbc5c68317
Adriaan Mulder <adriaan.mulder@gmail.com>
2018-03-30 23:56:56 -0700

    Add not found page.

---
+++ app/components/pages/home/base.js
@@ -0,0 +1,7 @@
+export const Home = ({url, modals = {}}) =>
+  <div>
+    <a href='/dne'>DNE</a>
+    <h1>Home</h1>
+  </div>
+
+export default Home
--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import wasmuth from 'wasmuth'
 
+import NotFound from '/components/pages/not-found'
 import Main from '/apps/main'
 import PageNotification from '/components/elements/page-notification'
 import Modals from '/components/modals'
@@ -15,6 +16,7 @@ const MainApp = () =>
   <div className='main-app-container' >
     <PageNotification />
     <Main />
+    <NotFound />
     <Modals />
   </div>
 
